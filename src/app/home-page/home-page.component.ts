ba62dcf2f9c53e568b01b9e0f57e31d3e52da5a6
lilu <>
2021-08-02 15:00:05 +0800

    вынесла логику в сервис

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -7,64 +7,31 @@ import {ProductsService} from "../services/products.service";
   template: `
       <div><h1>Список товаров</h1></div>
       <button>сортировать:</button>
-      <app-button-sort [ngStyle]="{backgroundColor:'title'===this.active?'lightsalmon':'white'}" (click)="orderBy('title')" [text]="'по наименованию'"></app-button-sort>
-      <app-button-sort [ngStyle]="{backgroundColor:'company'===this.active?'lightsalmon':'white'}" (click)="orderBy('company')" [text]="'по производителю'"></app-button-sort>
-      <app-button-sort [ngStyle]="{backgroundColor:'price'===this.active?'lightsalmon':'white'}" (click)="orderBy('price')" [text]="'по цене'"></app-button-sort>
+      <app-button-sort [ngStyle]="{backgroundColor:'title'===this.productsService.active?'lightsalmon':'white'}" (click)="this.productsService.orderBy('title')" [text]="'по наименованию'"></app-button-sort>
+      <app-button-sort [ngStyle]="{backgroundColor:'company'===this.productsService.active?'lightsalmon':'white'}" (click)="this.productsService.orderBy('company')" [text]="'по производителю'"></app-button-sort>
+      <app-button-sort [ngStyle]="{backgroundColor:'price'===this.productsService.active?'lightsalmon':'white'}" (click)="this.productsService.orderBy('price')" [text]="'по цене'"></app-button-sort>
       <table>
           <tr>
               <th>Наименование</th>
               <th>Производитель</th>
               <th>Цена</th>
           </tr>
-          <tr *ngFor="let p of productsOnThisPage"><td>{{p.title}}</td><td>{{p.company}}</td><td>{{p.price|currency:'RUB':'symbol-narrow'}}</td></tr>
+          <tr *ngFor="let p of this.productsService.productsOnThisPage"><td>{{p.title}}</td><td>{{p.company}}</td><td>{{p.price|currency:'RUB':'symbol-narrow'}}</td></tr>
       </table>
-      <app-button [text]="'Загрузить еще'" (click)="addPages()" [isDisabled]="disabled"></app-button>  
+      <app-button [text]="'Загрузить еще'" (click)="this.productsService.addPages()" [isDisabled]="this.productsService.disabled"></app-button>  
       
   `,
   styles: [ 'td,th{border-color: darksalmon;border-bottom-style: solid;border-width: thin}','table{text-align: center;padding: 10px 10px 10px 10px;}','button{border-style: hidden; background-color: white;}'
   ]
 })
 export class HomePageComponent implements OnInit {
-  active: string = '';
-  page: number = 1;
-  disabled: boolean = false;
-  private queryParams = {};
-  productsOnThisPage: Array<ProductSB>  = [];
+
 
   constructor(public productsService:ProductsService) { }
 
   ngOnInit(): void {
-    this.productsService.getProducts$(this.queryParams).subscribe(result => this.productsOnThisPage = result.items);
-    this.productsService.products = this.productsOnThisPage;
-  }
-
-  addPages(){
-    console.log(this.page);
-    if (this.page < 6){
-      this.page++;
-      this.applyQuery({page: this.page.toString()});
-      this.productsService.getProducts$(this.queryParams).subscribe(result => this.productsOnThisPage = result.items);
-      this.productsService.products = this.productsService.products.concat(this.productsOnThisPage);
-    }
-    if (this.page === 6){
-      this.disabled = true;
-    }
-    console.log(this.productsService.products);
-  }
-
-  orderBy(item:string){
-    this.applyQuery({orderBy:item});
-    this.productsService.getProducts$(this.queryParams).subscribe(result => this.productsOnThisPage = result.items);
-    console.log(this.productsService.products);
-  }
-
-  public applyQuery(params:{[key:string]:string}):void{
-    if (params['orderBy']){
-      this.active = params['orderBy'];
-    }
-    this.queryParams = {
-      ... this.queryParams,
-      ... params};
+    this.productsService.getProducts$(this.productsService.queryParams).subscribe(result => this.productsService.productsOnThisPage = result.items);
+    this.productsService.products = this.productsService.productsOnThisPage;
   }
 
 }
--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -9,6 +9,11 @@ import {HttpParams} from "@angular/common/http";
 export class ProductsService {
   url =`${environment.api}/products`;
   public products: Array<ProductSB> = [];
+  queryParams = {};
+  active: string = '';
+  page: number = 1;
+  disabled: boolean = false;
+  productsOnThisPage: Array<ProductSB>  = [];
 
   constructor(private http:HttpService) { }
 
@@ -16,4 +21,34 @@ export class ProductsService {
     const params = new HttpParams({fromObject:queryParams});
     return this.http.get<ProductInfo>(this.url,{params});
   }
+
+  orderBy(item:string){
+    this.applyQuery({orderBy:item});
+    this.getProducts$(this.queryParams).subscribe(result => this.productsOnThisPage = result.items);
+    console.log(this.products);
+  }
+
+  public applyQuery(params:{[key:string]:string}):void{
+    if (params['orderBy']){
+      this.active = params['orderBy'];
+    }
+    this.queryParams = {
+      ... this.queryParams,
+      ... params};
+  }
+
+  addPages(){
+    console.log(this.page);
+    if (this.page < 6){
+      this.page++;
+      this.applyQuery({page: this.page.toString()});
+      this.getProducts$(this.queryParams).subscribe(result => this.productsOnThisPage = result.items);
+      this.products = this.products.concat(this.productsOnThisPage);
+    }
+    if (this.page === 6){
+      this.disabled = true;
+    }
+    console.log(this.products);
+  }
+
 }
